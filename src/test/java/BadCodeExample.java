6b7251b940540bb907675f530a4a187461a2a5d6
TYarova <tyarova@ukr.net>
2018-02-20 13:57:57 +0200

    Updated google search test

--- src/test/java/BadCodeExample.java
+++ src/test/java/BadCodeExample.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,34 +9,39 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Thread.interrupted;
 import static java.lang.Thread.sleep;
 
-@Test
 public class BadCodeExample {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello World!");
+    @Test
+    public void BadCode() throws InterruptedException {
+        //System.out.println("Hello World!");
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com/");
         driver.findElement(By.id("lst-ib")).sendKeys("selenium");
         driver.findElement(By.name("btnK")).click();
         //Assert.assertEquals(driver.getTitle(), "selenium - Пошук Google");
+        sleep(5000);
 
         List<WebElement> links = driver.findElements(By.xpath("//*[@id=\"rso\"]//div/div/div/h3/a"));
-        System.out.println(links.toString());
-        boolean x = false;
         boolean y = true;
+        boolean k = false;
+        int counter = 0;
         for (WebElement myElement : links) {
-            String link = myElement.getText().toLowerCase();
-            System.out.println(link);
-            if (link.contains("selenium")) {
-                x = true;
+            String linkTitle = myElement.getText().toLowerCase();
+            System.out.println(linkTitle);
+            if (linkTitle.contains("selenium")) {
+                counter++;
             }
-
-            sleep(5000);
-            Assert.assertEquals(x, y);
-            driver.close();
-
+            System.out.println(counter);
+            System.out.println(links.size());
+            if (counter == links.size()) {
+                k = true;
+            }
         }
+        Assert.assertEquals(k, y);
+        driver.close();
+
     }
 }
 
