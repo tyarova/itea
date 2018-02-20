6aef21304394e6c6e399c246eae69f77267a3647
TYarova <tyarova@ukr.net>
2018-02-20 21:08:26 +0200

    Updated Login test Before/After Class annotations

--- src/test/java/LoginToLinkedIn.java
+++ src/test/java/LoginToLinkedIn.java
@@ -3,19 +3,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
 
-
 public class LoginToLinkedIn {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+
     @Test
     public void successfulLoginTest() throws InterruptedException {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com/");
         String initialPageTitle = "LinkedIn: Log In or Sign Up";
-        Assert.assertEquals(driver.getTitle(), initialPageTitle,
-                "Login page title is wrong");
+        Assert.assertEquals(driver.getTitle(), initialPageTitle, "Login page title is wrong");
 
         WebElement emailField = driver.findElement(By.id("login-email"));
         //WebElement emailField = driver.findElement(By.xpath("//*[@id='login-email']"));
@@ -31,22 +42,14 @@ public class LoginToLinkedIn {
         WebElement settingsControl = driver.findElement(By.xpath("//*[@id='nav-settings__dropdown-trigger']"));
 
         //Assert.assertEquals(driver.getCurrentUrl(), initiaiPageUrl);
-        Assert.assertNotEquals(driver.getTitle(), initialPageTitle,
-                "Page title has NOT been changed after Login");
+        Assert.assertNotEquals(driver.getTitle(), initialPageTitle, "Page title has NOT been changed after Login");
         //Assert.assertFalse(driver.getTitle().equals(initialPageTitle), "Page title has NOT been changed after Login");
-        Assert.assertTrue(settingsControl.isDisplayed(),
-                "Settings drop down is not displayed");
-        Assert.assertNotEquals(driver.getCurrentUrl(), initiaiPageUrl,
-                "Page URL has NOT been changed after Login");
-
-        driver.close();
+        Assert.assertTrue(settingsControl.isDisplayed(), "Settings drop down is not displayed");
+        Assert.assertNotEquals(driver.getCurrentUrl(), initiaiPageUrl, "Page URL has NOT been changed after Login");
     }
 
     @Test
     public void negativeLoginTest() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.linkedin.com/");
-
         WebElement emailField = driver.findElement(By.id("login-email"));
         //WebElement emailField = driver.findElement(By.xpath("//*[@id='login-email']"));
         WebElement passwordField = driver.findElement(By.id("login-password"));
@@ -56,8 +59,6 @@ public class LoginToLinkedIn {
         signInButton.click();
 
         WebElement alertMessage = driver.findElement(By.xpath("//div[@id='global-alert-queue']//strong[not(text()='')]"));
-        Assert.assertTrue(alertMessage.isDisplayed(),
-                "Alert message is not displayed");
-        driver.close();
+        Assert.assertTrue(alertMessage.isDisplayed(), "Alert message is not displayed");
     }
 }
