505b173e467b52cba983cd7e02db6c35f124a9f1
TYarova <tyarova@ukr.net>
2018-02-22 17:08:29 +0200

    test for search on LinkedIn

--- src/test/java/LinkedInSearchTest.java
+++ src/test/java/LinkedInSearchTest.java
@@ -8,10 +8,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static java.lang.Thread.sleep;
 
 public class LinkedInSearchTest {
     WebDriver driver;
+
     @BeforeMethod
     public void beforeTest() {
         driver = new FirefoxDriver();
@@ -33,11 +36,30 @@ public class LinkedInSearchTest {
         Assert.assertTrue(settingsControl.isDisplayed(), "Settings drop down is not displayed");
 
         //search
-        //WebElement submitButton = driver.findElement(By.xpath("//*[@type='search-icon']"));
-        //WebElement searchLine = driver.findElement(By.xpath("//input[@placeholder='Search']"));
-        //div[contains(@class, 'search-result--person')]
-        //searchLine.sendKeys("hr");
-        //submitButton.click();
+        WebElement submitButton = driver.findElement(By.xpath("//*[@type='search-icon']"));
+        WebElement searchLine = driver.findElement(By.xpath("//input[@placeholder='Search']"));
+        searchLine.sendKeys("hr");
+        submitButton.click();
+        sleep(10000);
+
+        List<WebElement> links = driver.findElements(By.xpath("//li[@class='search-result search-result__occluded-item ember-view']"));
+        boolean y = true;
+        boolean k = false;
+        int counter = 0;
+        for (WebElement myElement : links) {
+            String linkTitle = myElement.getText().toLowerCase();
+            System.out.println(linkTitle);
+            if (linkTitle.contains("hr")) {
+                counter++;
+            }
+            System.out.println(counter);
+            System.out.println(links.size());
+            if (counter == links.size()) {
+                k = true;
+            }
+        }
+        Assert.assertEquals(k, y, "Some of the results do not contain search request");
+        Assert.assertEquals(counter, 10, "There are less than 10 results have been found");
 
     }
 
