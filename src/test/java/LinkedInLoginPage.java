f5a73e88cb2ebeeddf38927394e1d741c9cef898
TYarova <tyarova@ukr.net>
2018-03-05 17:21:16 +0200

    negative login test

--- src/test/java/LinkedInLoginPage.java
+++ src/test/java/LinkedInLoginPage.java
@@ -1,27 +1,46 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class LinkedInLoginPage {
+
+public class LinkedInLoginPage extends LinkedInBasePage {
     WebDriver driver;
 
     public LinkedInLoginPage(WebDriver driver) {
+        super(driver);
         this.driver = driver;
     }
+
     private WebElement emailField;
     private WebElement passwordField;
     private WebElement signInButton;
+    private WebElement alertMessage;
 
-    private void initElements(){
+    private void initElements() {
         emailField = driver.findElement(By.id("login-email"));
         passwordField = driver.findElement(By.id("login-password"));
         signInButton = driver.findElement(By.id("login-submit"));
     }
-    public void loginAs(String email, String password) {
+
+    private void initErrorElements() {
+        alertMessage = driver.findElement(By.xpath("//div[@id='global-alert-queue']//strong[not(text()='')]"));
+    }
+
+    public boolean isFailedLogIn() throws InterruptedException {
+        initErrorElements();
+        waitUntilElementIsClickable(alertMessage);
+        return alertMessage.isDisplayed();
+    }
+
+    public LinkedInBasePage loginAs(String email, String password) {
         initElements();
         emailField.sendKeys(email);
         passwordField.sendKeys(password);
         signInButton.click();
+        return new LinkedInBasePage(driver);
     }
+//    public void loginAs(String password){
+//        loginAs("12345", password);
+//    }
+
 }
